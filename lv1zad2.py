718326bc4bbb5416920e2a40350cf793b716cfbf
Patrik Tomšić <79262118+iTomsic@users.noreply.github.com>
2025-03-11 22:45:52 +0100

    Update lv1zad2.py

--- lv1zad2.py
+++ lv1zad2.py
@@ -1,14 +1,18 @@
-broj = float(input("Unesite ocjenu od 0.0 do 1.0: "))
+try:
+    broj = float(input("Unesite ocjenu od 0.0 do 1.0: "))
+except:
+    print("Greška pri unosu!")
 
-if(broj >= 0.9):
-    print("Ocjena je A")
+if(broj < 0 or broj > 1):
+    print("Unesena kriva ocjena!")
+elif(broj >= 0.9):
+    print("Ocjena je A.")
 elif(broj >= 0.8): 
-    print("Ocjena je B")
+    print("Ocjena je B.")
 elif(broj >= 0.7):
-    print("Ocjena je C")
+    print("Ocjena je C.")
 elif(broj >= 0.6):
-    print("Ocjena je D")
+    print("Ocjena je D.")
 elif(broj < 0.6):
-    print("Ocjena je F")
+    print("Ocjena je F.")
 
-#wip
