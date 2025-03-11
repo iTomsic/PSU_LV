c936339aa3b3d5e736a76912206918b1787553ff
Patrik Tomšić <79262118+iTomsic@users.noreply.github.com>
2025-03-11 13:55:33 +0100

    Create lv1zad2.py

---
+++ lv1zad2.py
@@ -0,0 +1,14 @@
+broj = float(input("Unesite ocjenu od 0.0 do 1.0: "))
+
+if(broj >= 0.9):
+    print("Ocjena je A")
+elif(broj >= 0.8): 
+    print("Ocjena je B")
+elif(broj >= 0.7):
+    print("Ocjena je C")
+elif(broj >= 0.6):
+    print("Ocjena je D")
+elif(broj < 0.6):
+    print("Ocjena je F")
+
+#wip
