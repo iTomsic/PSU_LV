e13a956a0097e1d72ee2182b8b51916f39fe8f18
Patrik Tomšić <79262118+iTomsic@users.noreply.github.com>
2025-03-30 20:45:58 +0200

    Delete zad3.py

--- zad3.py
+++
@@ -1,9 +0,0 @@
-# Prepravite skriptu tako da rezultat izvođenja skripte bude sljedeći:
-# - vektori MSEtrain i MSEtest koji sadrži srednju kvadratnu pogrešku na podacima za učenje i podacima za testiranje
-# za tri različita stupnja dodatnih veličina u modelu (npr., degree= 2, 6 i 15)
-#
-# - slika na kojoj je usporedba izlaza modela za tri različita stupnja dodatnih veličina u modelu (npr., degree 2, 6 i
-# 15) s pozadinskom funkcijom koja je generirala podatke
-#
-# Što se događa s ovim rezultatima ako je na raspolaganju veći odnosno manji broj uzoraka za učenje? Simulirajte.
-
