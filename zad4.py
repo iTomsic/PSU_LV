0a4fcd181119822e73293618fb614e1b6df37a18
Patrik Tomšić <79262118+iTomsic@users.noreply.github.com>
2025-03-30 20:35:57 +0200

    LV4 start

---
+++ zad4.py
@@ -0,0 +1,29 @@
+# Primjer 4.3. učitava skup podataka koji se nalazi u datoteci car_processed.csv u dataframe. Ova datoteka sadrži
+# podatke koji su prikupljeni s online platforme cardekho.com koja se bavi prodajom vozila u Indiji. Za svaki automobil na
+# raspolaganju je:
+#
+# name: ime automobila
+# year: godina proizvodnje automobila
+# selling_price: cijena po kojoj se automobil prodaje (logaritam cijene u indijskim rupijima)
+# km_driven: prijeđeni broj kilometara
+# fuel: tip motora (Diesel, Petrol, CNG, LPG)
+# seller_type: prodavač (individual/dealer)
+# transmission: tip mjenjača (automatic/manual)
+# owner: broj prethodnih vlasnika
+# mileage: potrošnja automobila
+# engine: zapremnina motora u cm3
+# max_power: maksimalna snaga motora (bhp)
+# seats broj sjedala
+#
+# Skripta crta određene grafove kako bi se dobio uvid u dataset. Proučite dobivene grafove te odgovorite na sljedeća pitanja
+# 1. Koliko mjerenja (automobila) je dostupno u datasetu?
+# 2. Kakav je tip pojedinog stupca u dataframeu?
+# 3. Koji automobil ima najveću cijenu, a koji najmanju?
+# 4. Koliko automobila je proizvedeno 2012. godine?
+# 5. Koji automobil je prešao najviše kilometara, a koji najmanje?
+# 6. Koliko najčešće automobili imaju sjedala?
+# 7. Kolika je prosječna prijeđena kilometraža za automobile s dizel motorom, a koliko za automobile s benzinskim
+# motorom?
+
+
+
