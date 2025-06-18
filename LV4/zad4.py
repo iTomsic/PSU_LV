300bf765e973ee1b9bb725806b386e3c824ed384
Patrik Tomšić <79262118+iTomsic@users.noreply.github.com>
2025-06-18 18:07:28 +0200

    Update zad4.py

--- LV4/zad4.py
+++ LV4/zad4.py
@@ -25,5 +25,33 @@
 # 7. Kolika je prosječna prijeđena kilometraža za automobile s dizel motorom, a koliko za automobile s benzinskim
 # motorom?
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_csv('cars_processed.csv')
+print(df.info())
+
+sns.pairplot(df, hue='fuel')
+
+sns.relplot(data=df, x='km_driven', y='selling_price', hue='fuel')
+df = df.drop(['name','mileage'], axis=1)
+
+obj_cols = df.select_dtypes(object).columns.values.tolist()
+num_cols = df.select_dtypes(np.number).columns.values.tolist()
+
+fig = plt.figure(figsize=[15,8])
+for col in range(len(obj_cols)):
+    plt.subplot(2,2,col+1)
+    sns.countplot(x=obj_cols[col], data=df)
+
+df.boxplot(by ='fuel', column =['selling_price'], grid = False)
+
+df.hist(['selling_price'], grid = False)
+
+tabcorr = df.drop(['fuel','seller_type','transmission','owner'], axis = 1).corr()
+sns.heatmap(df.drop(['fuel','seller_type','transmission','owner'], axis = 1).corr(), annot=True, linewidths=2, cmap= 'coolwarm') 
+
+plt.show()
 
